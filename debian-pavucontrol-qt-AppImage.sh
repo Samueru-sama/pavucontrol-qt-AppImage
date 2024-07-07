5b94b4b42a4af528dee4f235dd1909c52591d03c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-07-07 11:10:39 -0400

    Update debian-pavucontrol-qt-AppImage.sh

--- debian-pavucontrol-qt-AppImage.sh
+++ debian-pavucontrol-qt-AppImage.sh
@@ -10,7 +10,7 @@ EXEC="$APP"
 ICON="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/yast-sound.svg"
 
 LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
-APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
+APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
 
 # CREATE DIRECTORIES
 [ -n "$APP" ] && mkdir -p ./"$APP/$APPDIR"/tmp && cd ./"$APP/$APPDIR"/tmp || exit 1
