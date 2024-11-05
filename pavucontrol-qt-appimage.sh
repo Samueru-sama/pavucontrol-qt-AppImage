e7f9d87ed12117bb29801a1e9e1684f706a89f4a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-05 06:26:54 -0400

    Update and rename pavucontrol-qt-AppImage.sh to pavucontrol-qt-appimage.sh

--- pavucontrol-qt-appimage.sh
+++ pavucontrol-qt-appimage.sh
@@ -6,20 +6,23 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 APP=pavucontrol-qt
 ICON="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/yast-sound.svg"
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|rofi-AppImage|continuous|*$ARCH.AppImage.zsync"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 SHARUN="https://github.com/VHSgunzo/sharun/releases/download/v0.0.2/sharun-$ARCH"
 
 # Prepare AppDir
-mkdir -p ./"$APP"/AppDir
+mkdir -p ./"$APP"/AppDir/usr/share/applications
 cd ./"$APP"/AppDir
 
-mv /usr/share/applications/pavucontrol-qt.desktop ./
+cp /usr/share/applications/pavucontrol-qt.desktop ./usr/share/applications
+cp /usr/share/applications/pavucontrol-qt.desktop ./
 wget "$ICON" -O multimedia-volume-control.svg
-ln -s ./multimedia-volume-control.png ./.DirIcon
+ln -s ./multimedia-volume-control.svg ./.DirIcon
 
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
+export QT_PLUGIN_PATH="$CURRENTDIR"/shared/lib/qt6/plugins
 "$CURRENTDIR/bin/pavucontrol-qt" "$@"
 EOF
 chmod +x ./AppRun
@@ -31,18 +34,24 @@ chmod +x ./lib4bin ./sharun
 HARD_LINKS=1 ./lib4bin "$(command -v pavucontrol-qt)"
 rm -f ./lib4bin
 
+# DELOY QT
+mkdir -p ./shared/lib/qt6
+cp -r /usr/lib/qt6/plugins ./shared/lib/qt6
+ldd ./shared/lib/qt6/plugins/*/* \
+  | awk -F"[> ]" '{print $4}' | xargs -I {} cp -nv {} ./shared/lib
+
 rm -f ./shared/lib/lib.path || true # forces sharun to regenerate the file
-VERSION=$(./AppRun --version | awk 'FNR==1 {print $2; exit}')
+export VERSION=$(./AppRun --version | awk 'FNR==1 {print $2; exit}')
 
 # MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
-wget -q "$APPIMAGETOOL" -O ./appimagetool 
+cd ..
+wget -q "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
 
 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|rofi-AppImage|continuous|*$ARCH.AppImage.zsync" \
-	./"$APP".AppDir "$APP"-"$VERSION"-"$ARCH".AppImage
+	-n -u "$UPINFO" "$PWD"/AppDir "$PWD"/"$APP"-"$VERSION"-"$ARCH".AppImage
 
-mv ./*.AppImage ../
-cd .. 
+mv ./*.AppImage* ../
+cd ..
 rm -rf ./"$APP"
 echo "All Done!"
